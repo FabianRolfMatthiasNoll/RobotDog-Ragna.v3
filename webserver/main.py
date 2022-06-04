5505ab38cc840c08d7ebe1f759cd9896b18d3a73
Fabian Noll <Fabian.Noll@googlemail.com>
2022-06-04 09:06:21 +0200

    added basic controll commands

--- webserver/main.py
+++ webserver/main.py
@@ -4,23 +4,57 @@ import sys
 sys.path.append('..')
 
 from typing import Union
-
 from fastapi import FastAPI
-
 import robot.ragna_program as ragna
 
 app = FastAPI()
 
-@app.get("/move")
+@app.get("/move_forward")
 def read_root():
-    ragna.leftFrontLeg.moveUp()
-
-    ragna.time.sleep(1)
-
-    ragna.leftFrontLeg.moveDown()
-
+    ragna.robot.moveForward()
     return {}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/move_backwards")
+def read_root():
+    ragna.robot.moveBackwards()
+    return {}
+
+@app.get("/turn_Right")
+def read_root():
+    ragna.robot.turnRight()
+    return {}
+
+@app.get("/turn_Left")
+def read_root():
+    ragna.robot.turnLeft()
+    return {}
+
+@app.get("/move_Right")
+def read_root():
+    ragna.robot.moveRight()
+    return {}
+
+@app.get("/move_Left")
+def read_root():
+    ragna.robot.moveLeft
+    return {}
+
+@app.get("/sit")
+def read_root():
+    ragna.robot.sit
+    return {}
+
+@app.get("/standUp")
+def read_root():
+    ragna.robot.standUp
+    return {}
+
+@app.get("/layDown")
+def read_root():
+    ragna.robot.layDown
+    return {}
+
+@app.get("/wiggle")
+def read_root():
+    ragna.robot.wiggle
+    return {}

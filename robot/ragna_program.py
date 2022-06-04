d91b49cd92354429d711d89deccac3f5b8a2b76e
Fabian Noll <Fabian.Noll@googlemail.com>
2022-06-04 19:15:37 +0200

    calibrated Servos and cleaned code

--- robot/ragna_program.py
+++ robot/ragna_program.py
@@ -8,11 +8,7 @@ from adafruit_pca9685 import PCA9685
 
 # diverse imports for board and coding managment
 from board import SCL, SDA
-from gpiozero import LED
 import busio
-
-# initialising GPIO17 as LED
-led = LED(17)
 
 # setting up the i2c bus
 i2c = busio.I2C(SCL, SDA)
@@ -26,39 +22,24 @@ controllerRight.frequency = 50
 
 # initializing all robot legs
 leftFrontLeg = models.Leg(
-        [servo.Servo(controllerLeft.channels[0], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerLeft.channels[1], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerLeft.channels[2], min_pulse = 400, max_pulse = 2500)])
+        [servo.Servo(controllerLeft.channels[0], min_pulse = 800, max_pulse = 2450),
+         servo.Servo(controllerLeft.channels[1], min_pulse = 800, max_pulse = 2450),
+         servo.Servo(controllerLeft.channels[2], min_pulse = 800, max_pulse = 2450)])
 
 leftBackLeg = models.Leg(
-        [servo.Servo(controllerLeft.channels[13], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerLeft.channels[14], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerLeft.channels[15], min_pulse = 400, max_pulse = 2500)])
+        [servo.Servo(controllerLeft.channels[13], min_pulse = 800, max_pulse = 2450),
+         servo.Servo(controllerLeft.channels[14], min_pulse = 800, max_pulse = 2450),
+         servo.Servo(controllerLeft.channels[15], min_pulse = 800, max_pulse = 2450)])
 
 rightFrontLeg = models.Leg(
-        [servo.Servo(controllerRight.channels[13], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerRight.channels[14], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerRight.channels[15], min_pulse = 400, max_pulse = 2500)])
+        [servo.Servo(controllerRight.channels[13], min_pulse = 800, max_pulse = 2450),
+         servo.Servo(controllerRight.channels[14], min_pulse = 800, max_pulse = 2450),
+         servo.Servo(controllerRight.channels[15], min_pulse = 800, max_pulse = 2450)])
 
 rightBackLeg = models.Leg(
-        [servo.Servo(controllerRight.channels[0], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerRight.channels[1], min_pulse = 400, max_pulse = 2500),
-         servo.Servo(controllerRight.channels[2], min_pulse = 400, max_pulse = 2500)])
+        [servo.Servo(controllerRight.channels[0], min_pulse = 800, max_pulse = 2450),
+         servo.Servo(controllerRight.channels[1], min_pulse = 780, max_pulse = 2450),
+         servo.Servo(controllerRight.channels[2], min_pulse = 800, max_pulse = 2450)])
 
 # initializing robot
 robot = models.Robot(leftFrontLeg,leftBackLeg,rightFrontLeg,rightBackLeg)
-
-# to be added: a while loop tahts always true, at the start turn on a led to show that the robot is ready
-
-#led.on()
-
-# testing area:
-#leftFrontLeg.moveUp()
-
-#time.sleep(1)
-
-#leftFrontLeg.moveDown()
-
-#print(leftFrontLeg.alpha)
-#print(leftFrontLeg.beta)
-#print(leftFrontLeg.gamma)
